b480fc70110c642dd5289bca08a642508fa2e441
Ivan Vaschev <49611466+ivanvvsch@users.noreply.github.com>
2021-03-12 21:35:37 +0300

    Add files via upload
    Version 0.1

---
+++ shortener_ver01.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import pyshorteners
+import pyperclip
+
+window = Tk()
+s = pyshorteners.Shortener()
+
+window.geometry('540x540')
+window.title("URL Shortener by VVSCH")
+window.resizable(height = False, width = False)
+
+label1 = Label(window, text = 'URL Shortener', font = ("Arial", 50))
+label1.pack()
+
+getLink = Entry(window, bg = 'grey')
+getLink.pack()
+
+def makeShortLink():
+    shortLink = s.tinyurl.short(getLink.get())
+    label2 = Label(window, text = shortLink, font = ("Arial", 20))
+    label2.pack()
+    label3 = Label(window, text = 'Ссылка скопирована!', font = ("Arial", 10))
+    label3.pack()
+    pyperclip.copy(shortLink)
+
+btn1 = Button(window, bg = 'red', text = 'Сократить ссылку', command = makeShortLink)
+btn1.pack()
+
+window.mainloop()
